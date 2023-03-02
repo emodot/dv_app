3150c87f2501093c99c1b19a37676467c5afb68d
David Emaye <emayeodavid@gmail.com>
2023-03-03 00:54:41 +0100

    set up app structure

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0x003e3fd7);
+const kPrimaryColor2 = Color(0x00b83ed7);
+const kBackgroundColor = Color(0xFFF9F8FD);
+
+const double kDefaultPadding = 20.0;
