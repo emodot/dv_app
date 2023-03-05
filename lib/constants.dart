f34aae7f599d9da71177e2436b1af8b6ab721381
David Emaye <emayeodavid@gmail.com>
2023-03-05 22:09:48 +0100

    developing the onboarding screens

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0x003e3fd7);
-const kPrimaryColor2 = Color(0x00b83ed7);
+const kPrimaryColor = Color.fromARGB(255, 62, 62, 215);
+const kPrimaryColorMid = Color.fromARGB(255, 123, 62, 215);
+const kPrimaryColor2 = Color.fromARGB(255, 184, 62, 215);
 const kBackgroundColor = Color(0xFFF9F8FD);
 
 const double kDefaultPadding = 20.0;
