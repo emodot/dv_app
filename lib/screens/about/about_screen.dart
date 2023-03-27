4f978ed3a6c759dbaee7c9f389547fca1bd37a73
David Emaye <emayeodavid@gmail.com>
2023-03-27 01:45:07 +0100

    updated about page

--- lib/screens/about/about_screen.dart
+++ lib/screens/about/about_screen.dart
@@ -1,9 +1,5 @@
-import 'package:dv_app/components/bottom_nav_bar.dart';
-import 'package:dv_app/components/custom_app_bar.dart';
-import 'package:dv_app/components/side_nav_bar.dart';
 import 'package:dv_app/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({super.key});
@@ -32,18 +28,21 @@ class AboutScreen extends StatelessWidget {
               RichText(
                 textAlign: TextAlign.center,
                 text: const TextSpan(
-                  style: TextStyle(
-                    color: kTextColor,
-                    height: 2
-                  ),
+                  style: TextStyle(color: kTextColor, height: 2),
                   children: [
                     TextSpan(
-                      text:
-                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mollis neque vel urna accumsan pretium. Vestibulum sed massa et justo hendrerit cursus.Nullam vel felis in ante accumsan consectetur adipiscing elit. Nulla mollis neque vel urna accumsan pretium. Vestibulum sed massa et justo hendrerit cursus.Nullam vel felis in ante accumsan",
+                      text: 'There is a popular saying that "Health is Wealth" \n',
+                    ),
+                    TextSpan(
+                      text: "Our society right now is faced with the challenge of dealing with fake drugs which have taken over our markets and even going to the extent of claiming innocent lives. We saw this as a problem to our society and decided to rise to the challenges to limit its destruction to lives. \n",
                     ),
                     TextSpan(
                       text:
-                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mollis neque vel urna accumsan pretium. Vestibulum sed massa et justo hendrerit cursus.Nullam vel felis in ante accumsan consectetur adipiscing elit. Nulla mollis neque vel urna accumsan pretium. Vestibulum sed massa et justo hendrerit cursus.Nullam vel felis in ante accumsan",
+                          'We decided to provide a solution to this challenge which is in line with the United Nations Sustainable Development Goal (SDG) No. 3: Good Health and Well-being i.e. Ensuring healthy lives and promoting well-being for all and at all ages which is essential to sustainable development. We arrived at a solution by developing this App and named it "DV App" (Drug Verification app). \n',
+                    ),
+                    TextSpan(
+                      text:
+                          'We designed and built this app for everyone in Nigeria to be able to verify their drugs upon purchase from any drug store or pharmacy, if the drugs they are about to consume is void of harm and if it is fully endorsed by the National Agency of Foods and Drugs Administration Company (NAFDAC) which is concerned with the regularization of consumables.',
                     ),
                   ],
                 ),
